88d9281abffd6152de1713396035af2c5a6095ed
Leendert de Kok <koktaildotcom@hotmail.com>
2018-03-04 11:50:35 +0100

    refactoring to chained promises

--- drivers/mi_flora/driver.js
+++ drivers/mi_flora/driver.js
@@ -11,11 +11,157 @@ const REALTIME_CHARACTERISTIC_UUID = '00001a0000001000800000805f9b34fb';
 
 class MiFloraDriver extends Homey.Driver {
 
-    onInit() {
-        this.connectDevices(this.getDevices());
+    getAdvertisements(device) {
+        return new Promise((resolve, reject) => {
+            Homey.ManagerBLE.discover().then(function (advertisements) {
+                advertisements.forEach(function (advertisement) {
+                    if (advertisement.uuid === device.getData().uuid) {
+                        resolve(advertisement);
+                    }
+                });
+            });
+        });
     }
 
-    connectDevices(devices) {
+    getPeripheral(advertisement) {
+        return new Promise((resolve, reject) => {
+            advertisement.connect((error, peripheral) => {
+                if (error) {
+                    reject('failed connection to peripheral: ' + error);
+                }
+                resolve(peripheral);
+            });
+        });
+    }
+
+    getData(peripheral) {
+        return new Promise((resolve, reject) => {
+
+            peripheral.discoverServices((error, services) => {
+                if (error) {
+                    reject("failed connection to services: " + error);
+                }
+
+                console.log('got services!');
+                services.forEach(function (service) {
+
+                    service.discoverCharacteristics((error, characteristics) => {
+
+                        if (error) {
+                            reject("failed connection to services: " + error);
+                        }
+
+                        if (characteristics) {
+                            characteristics.forEach(function (characteristic) {
+                                console.log('got characteristic ' + characteristic.uuid);
+
+                                switch (characteristic.uuid) {
+
+                                    case DATA_CHARACTERISTIC_UUID:
+                                        characteristic.read(function (error, data) {
+
+                                            if (error) {
+                                                reject("failed connection to services: " + error);
+                                            }
+
+                                            console.log('read characteristic ' + characteristic.uuid);
+
+                                            console.log(data);
+
+                                            let temperature = data.readUInt16LE(0) / 10;
+                                            let lux = data.readUInt32LE(3);
+                                            let moisture = data.readUInt16BE(6);
+                                            let fertility = data.readUInt16LE(8);
+
+                                            console.log('temperature: %s °C', temperature);
+                                            console.log('Light: %s lux', lux);
+                                            console.log('moisture: %s %', moisture);
+                                            console.log('fertility: %s µS/cm', fertility);
+
+                                            resolve({
+                                                "temperature": temperature,
+                                                "lux": lux,
+                                                "moisture": moisture,
+                                                "fertility": fertility
+                                            });
+                                        });
+                                        break;
+                                    // case FIRMWARE_CHARACTERISTIC_UUID:
+                                    //
+                                    //     console.log('read characteristic ' + characteristic.uuid);
+                                    //     characteristic.read(function (error, data) {
+                                    //
+                                    //         if (error) {
+                                    //             console.log("failed read characteristic: %s", error);
+                                    //         }
+                                    //
+                                    //         let batteryLevel = parseInt(data.toString('hex', 0, 1), 16);
+                                    //         let firmwareVersion = data.toString('ascii', 2, data.length);
+                                    //
+                                    //         console.log('batteryLevel: %s %', batteryLevel);
+                                    //         console.log('firmwareVersion: %s ', firmwareVersion);
+                                    //     });
+                                    //
+                                    //     break;
+                                    case REALTIME_CHARACTERISTIC_UUID:
+                                        console.log('enabling realtime');
+                                        characteristic.write(Buffer.from([0xA0, 0x1F]), false);
+                                        break;
+                                    default:
+                                    // //console.log('found characteristic uuid %s but not matched the criteria', characteristic.uuid);
+                                }
+                            })
+                        }
+                    });
+                });
+            });
+        });
+    }
+
+    onInit() {
+
+        let driver = this;
+
+        console.log('getData start');
+        this.getDevices().forEach(function (device) {
+            driver.getAdvertisements(device)
+                .then((advertisement) => {
+                    console.log('getAdvertisements success');
+                    return driver.getPeripheral(advertisement);
+                }).catch(error => {
+
+                console.log("Cannot get advertisement", error);
+            })
+                .then((peripheral) => {
+                    console.log('got peripheral');
+                    return driver.getData(peripheral);
+                }).catch(error => {
+
+                console.log("Cannot get data", error);
+            })
+                .then((data) => {
+                    console.log('getData success');
+                    console.log(data);
+                }).catch(error => {
+
+                console.log("Cannot get advertisement", error);
+            })
+                .then(() => {
+                    // simulate `finally` clause
+                    console.log('clean up');
+                    console.log('disconnect Advertisements');
+                    // return driver.disconnectAdvertisements(advertisement);
+                });
+        });
+
+    }
+
+
+    // onInit() {
+    //     this.connectDevicesX(this.getDevices());
+    // }
+
+    connectDevicesX(devices) {
         Homey.ManagerBLE.discover().then(function (advertisements) {
             advertisements.forEach(function (advertisement) {
                 devices.forEach(function (device) {
